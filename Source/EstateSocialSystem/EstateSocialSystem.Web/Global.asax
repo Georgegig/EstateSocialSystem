067556bcee37fbc2a52e3b59774a6581ca9eecca
Georgegig <george_gig@abv.bg>
2016-02-03 20:28:52 +0200

    Initial commit

---
+++ Source/EstateSocialSystem/EstateSocialSystem.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EstateSocialSystem.Web.MvcApplication" Language="C#" %>
---
+++ Source/EstateSocialSystem/EstateSocialSystem.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EstateSocialSystem.Web.Startup))]
+namespace EstateSocialSystem.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
