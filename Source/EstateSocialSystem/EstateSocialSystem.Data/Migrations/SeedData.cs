3e5fc3202549e12cbb2a40d47cb6c630753a7d89
Georgegig <george_gig@abv.bg>
2016-02-05 20:08:14 +0200

    SeedData.cs file added

---
+++ Source/EstateSocialSystem/EstateSocialSystem.Data/Migrations/SeedData.cs
@@ -0,0 +1,21 @@
+﻿namespace EstateSocialSystem.Data.Migrations
+{
+    using Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SeedData
+    {
+        public static Random Rand = new Random();
+        
+        public User Author { get; set; }
+
+        public SeedData(User author)
+        {
+
+        }
+    }
+}
