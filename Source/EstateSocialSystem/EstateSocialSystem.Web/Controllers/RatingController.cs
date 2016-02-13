27f6fb595249145dcb29baaee47bac9c4db2923c
Georgegig <george_gig@abv.bg>
2016-02-13 14:07:34 +0200

    Added RatingController

---
+++ Source/EstateSocialSystem/EstateSocialSystem.Web/Controllers/RatingController.cs
@@ -0,0 +1,13 @@
+﻿namespace EstateSocialSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class RatingController : Controller
+    {
+        // GET: Rating
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
