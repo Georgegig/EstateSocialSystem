7c3e56fd43698ac1a27a32cdb015d9552330df34
Georgegig <george_gig@abv.bg>
2016-02-21 21:32:22 +0200

    Question Author Added.

--- Source/EstateSocialSystem/EstateSocialSystem.Web/Areas/Forum/ViewModels/Questions/QuestionDisplayViewModel.cs
+++ Source/EstateSocialSystem/EstateSocialSystem.Web/Areas/Forum/ViewModels/Questions/QuestionDisplayViewModel.cs
@@ -1,12 +1,25 @@
 ﻿namespace EstateSocialSystem.Web.Areas.Forum.ViewModels
 {
     using Data.Models;
+    using System;
     using Web.Infrastructure.Mapping;
+    using AutoMapper;
 
-    public class QuestionDisplayViewModel : IMapFrom<Post>
+    public class QuestionDisplayViewModel : IMapFrom<Post>, IHaveCustomMappings
     {
         public string Title { get; set; }
 
         public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<Post, QuestionDisplayViewModel>()
+                .ForMember(x => x.Author,
+                opt => opt.MapFrom(x => x.Author.UserName));
+        }
     }
 }
