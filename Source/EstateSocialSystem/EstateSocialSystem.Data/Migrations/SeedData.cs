108a7f01dc4ebcbaee6dc9148f4998614c8b18cc
Georgegig <george_gig@abv.bg>
2016-02-24 10:57:21 +0200

    Changes and updates - cosmetic

--- Source/EstateSocialSystem/EstateSocialSystem.Data/Migrations/SeedData.cs
+++ Source/EstateSocialSystem/EstateSocialSystem.Data/Migrations/SeedData.cs
@@ -23,67 +23,7 @@
                 {
                     RoleManager.Create(new IdentityRole(roles[i]));
                 }
-            }
-
-            //// var regularUser = new User()
-            //// {
-            ////     UserName = this.users[1],
-            ////     FirstName = "Regular",
-            ////     LastName = "RegularUser",
-            ////     Email = this.users[1],
-            ////     IsManufacturer = false
-            //// };
-            //// 
-            //// var manufacturerUser = new User()
-            //// {
-            ////     UserName = this.users[0],
-            ////     FirstName = "Manufacturer",
-            ////     LastName = "ManufacturerUser",
-            ////     Email = this.users[0],
-            ////     IsManufacturer = true
-            //// };
-            ////
-            //// var administratorUser = new User()
-            //// {
-            ////     UserName = this.users[2],
-            ////     FirstName = "Administrator",
-            ////     LastName = "AdministratorUser",
-            ////     Email = this.users[2],
-            ////     IsManufacturer = false
-            //// };
-            ////
-            //// context.Users.Add(regularUser);
-            //// context.Users.Add(manufacturerUser);
-            //// context.Users.Add(administratorUser);
-            //// context.SaveChanges();
-            ////
-            //// regularUser.Roles.Add(new IdentityUserRole()
-            //// {
-            ////     RoleId = RoleManager.Roles.First(r => r.Name == "Regular").Id,
-            ////     UserId = regularUser.Id
-            //// });
-            ////
-            //// administratorUser.Roles.Add(new IdentityUserRole()
-            //// {
-            ////     RoleId = RoleManager.Roles.First(r => r.Name == "Administrator").Id,
-            ////     UserId = regularUser.Id
-            //// });
-            ////
-            //// manufacturerUser.Roles.Add(new IdentityUserRole()
-            //// {
-            ////     RoleId = RoleManager.Roles.First(r => r.Name == "Manufacturer").Id,
-            ////     UserId = regularUser.Id
-            //// });
-            ////
-            //// context.SaveChanges();
-
-            ////Seed Ratings
-
-            ////Seed Appliances
-
-            ////Seed Estates
-
-            ////Seed Users
+            }            
 
             //// FORUM SEEDING 
             if (!context.Answers.Any())
